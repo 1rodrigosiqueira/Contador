b102c05f3bc29789058af2ff59dd1ce19fd9aa0a
Rodrigo Siqueira <101567492+1rodrigosiqueira@users.noreply.github.com>
2022-03-20 19:14:10 -0300

    first commit

---
+++ Contador/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaClock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criação de variáveis
+
+            int Segundo = 0;
+            int Minuto = 0;
+            int Hora = 0;
+
+            // Programa
+            while (Segundo <= 60)
+            {
+                //   Console.Clear();
+                Console.WriteLine(Hora + ":" + Minuto + ":" + Segundo);
+                Segundo++;
+                //Console.ReadLine();
+                if (Segundo == 60)
+                {
+                    Segundo = 0;
+                    Minuto++;
+                    if (Minuto == 60)
+                    {
+                        Segundo = 0;
+                        Minuto = 0;
+                        Hora++;
+                        if (Hora == 24)
+                        {
+                            Segundo = 0;
+                            Minuto = 0;
+                            Hora = 0;
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+Primeiro programa criado em c#;
+
+Trata-se de um contador simples no estilo relógio usando IF e While;
+
+Ele corre como um cronômetro.
+
+grato 
+
+Rodrigo Siqueira de Oliveira
